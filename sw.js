c36cbed694a08837f5885b9ec9ff40c8158089f8
Richard Hanna <supertanuki@gmail.com>
2016-12-16 17:12:56 +0100

    Fetch request in service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-var cacheName = 'rsspwa-v1';
+var appCacheName = 'rsspwa-v1';
+var dataCacheName = 'rsspwa-data';
 
 var filesToCache = [
     '/',
@@ -8,7 +9,7 @@ var filesToCache = [
 
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open(cacheName).then(function(cache) {
+        caches.open(appCacheName).then(function(cache) {
             console.log('[ServiceWorker] Caching app shell');
             return cache.addAll(filesToCache);
         })
@@ -21,7 +22,7 @@ self.addEventListener('activate', function(event) {
     event.waitUntil(
         caches.keys().then(function(keyList) {
             return Promise.all(keyList.map(function(key) {
-                if (key !== cacheName) {
+                if (key !== appCacheName && key !== dataCacheName) {
                     console.log('[ServiceWorker] Removing old cache', key);
                     return caches.delete(key);
                 }
@@ -30,4 +31,39 @@ self.addEventListener('activate', function(event) {
     );
 
     return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log('[Service Worker] Fetch', event.request.url);
+
+    var myApp = 'http://127.0.0.1:8887';
+
+    if (event.request.url.indexOf(myApp) > -1) {
+        /*
+         * The app is asking for app shell files. In this scenario the app uses the
+         * "Cache, falling back to the network" offline strategy:
+         * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+         */
+        event.respondWith(
+            caches.match(event.request).then(function(response) {
+                return response || fetch(event.request);
+            })
+        );
+    } else {
+        /*
+         * When the request URL contains dataUrl, the app is asking for fresh
+         * weather data. In this case, the service worker always goes to the
+         * network and then caches the response. This is called the "Cache then
+         * network" strategy:
+         * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+         */
+        event.respondWith(
+            caches.open(dataCacheName).then(function(cache) {
+                return fetch(event.request).then(function(response) {
+                    cache.put(event.request.url, response.clone());
+                    return response;
+                });
+            })
+        );
+    }
 });
