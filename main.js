fccd11a8b92f3e623217231767d684b96b4204f3
Richard Hanna <supertanuki@gmail.com>
2016-12-16 16:41:36 +0100

    Add strict mode

--- main.js
+++ main.js
@@ -1,46 +1,49 @@
-function ajaxRequest(url) {
-    var request = new XMLHttpRequest();
+(function () {
+    'use strict';
 
-    request.onloadend = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                showPosts(request.response);
+    function ajaxRequest (url) {
+        var request = new XMLHttpRequest();
+
+        request.onloadend = function () {
+            if (request.readyState === XMLHttpRequest.DONE) {
+                if (request.status === 200) {
+                    showPosts(request.response);
+                }
             }
-        }
-    };
+        };
 
-    request.onerror = function () {
-        console.log(request.readyState);
-    };
+        request.onerror = function () {
+            console.log(request.readyState);
+        };
 
-    request.open('GET', url);
-    request.send();
-}
-
-function showPosts(rss)
-{
-    if (window.DOMParser) {
-        var parser = new DOMParser();
-        var xmlDoc = parser.parseFromString(rss, "text/xml");
-        var titleList = [];
-        var html = '';
-
-        [].forEach.call(xmlDoc.getElementsByTagName("item"), function (element) {
-            titleList.push(element.getElementsByTagName('title')[0].textContent);
-        });
-
-        for (let title of titleList) {
-            html += '<li>' + title + '</li>';
-        }
-
-        html = '<ul>' + html + '</ul>';
-
-        document.getElementById('app').innerHTML = html;
-    } else {
-        console.log('method not supported');
+        request.open('GET', url);
+        request.send();
     }
-}
 
-var feed = 'http://www.lemonde.fr/rss/une.xml';
-ajaxRequest(feed);
+    function showPosts (rss) {
+        if (window.DOMParser) {
+            var parser    = new DOMParser();
+            var xmlDoc    = parser.parseFromString(rss, "text/xml");
+            var titleList = [];
+            var html      = '';
+
+            [].forEach.call(xmlDoc.getElementsByTagName("item"), function (element) {
+                titleList.push(element.getElementsByTagName('title')[0].textContent);
+            });
+
+            for (let title of titleList) {
+                html += '<li>' + title + '</li>';
+            }
+
+            html = '<ul>' + html + '</ul>';
+
+            document.getElementById('app').innerHTML = html;
+        } else {
+            console.log('method not supported');
+        }
+    }
+
+    var feed = 'http://www.lemonde.fr/rss/une.xml';
+    ajaxRequest(feed);
+}());
 
