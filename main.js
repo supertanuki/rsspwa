f1e5d3daab3cf71243c01ef160a0ab1da11f4728
Richard Hanna <supertanuki@gmail.com>
2016-12-16 16:15:01 +0100

    Show posts

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function ajaxRequest(url) {
     request.onloadend = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
-                console.log(request.response);
+                showPosts(request.response);
             }
         }
     };
@@ -17,5 +17,30 @@ function ajaxRequest(url) {
     request.send();
 }
 
+function showPosts(rss)
+{
+    if (window.DOMParser) {
+        var parser = new DOMParser();
+        var xmlDoc = parser.parseFromString(rss, "text/xml");
+        var titleList = [];
+        var html = '';
+
+        [].forEach.call(xmlDoc.getElementsByTagName("item"), function (element) {
+            titleList.push(element.getElementsByTagName('title')[0].textContent);
+        });
+
+        for (let title of titleList) {
+            html += '<li>' + title + '</li>';
+        }
+
+        html = '<ul>' + html + '</ul>';
+
+        document.getElementById('app').innerHTML = html;
+    } else {
+        console.log('method not supported');
+    }
+}
+
 var feed = 'http://www.lemonde.fr/rss/une.xml';
 ajaxRequest(feed);
+
