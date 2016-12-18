2deb511769ad0a59b2f7e6483415a94af7b93bc9
Richard Hanna <supertanuki@gmail.com>
2016-12-18 21:54:04 +0100

    Replace Xhr by Fetch and use api.rss2json.com

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ A Progressive Web App RSS reader with offline features ! 📵
 
 ## Credits
 
+Rss2json API, a great alternative to deprecated Google Feed API [https://rss2json.com](https://rss2json.com)
+
 Logo made with [https://logomakr.com](https://logomakr.com)
 
 Favicons made with [http://realfavicongenerator.net](http://realfavicongenerator.net)
--- app.js
+++ app.js
@@ -1,59 +1,49 @@
 (function () {
     'use strict';
 
+    const RSS2JSON_API_ENDPOINT = 'https://api.rss2json.com/v1/api.json?rss_url=';
+
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', function() {
             navigator.serviceWorker.register('sw.js');
         });
     }
 
-    function ajaxRequest (url) {
-        var request = new XMLHttpRequest();
+    function fetchFeed(url)
+    {
+        if (!'Request' in window) {
+            console.log('Request not supported');
+            return;
+        }
 
-        request.onloadend = function () {
-            if (request.readyState === XMLHttpRequest.DONE) {
-                if (request.status === 200) {
-                    showPosts(request.response);
-                }
-            }
-        };
+        var request = new Request(RSS2JSON_API_ENDPOINT + url);
 
-        request.onerror = function () {
-            console.log(request.readyState);
-        };
-
-        request.open('GET', url);
-        request.send();
+        fetch(request, {})
+            .then(function(response) {
+                console.log(response);
+                return response.json();
+            })
+            .then(function(json) {
+                showPosts(json);
+            })
+            .catch(function(error) {
+                console.log('Request failed = ', error);
+            });
     }
 
     function showPosts (rss) {
-        if (window.DOMParser) {
-            var parser    = new DOMParser();
-            var xmlDoc    = parser.parseFromString(rss, "text/xml");
-            var items = [];
-            var html      = '';
+        var html      = '';
 
-            [].forEach.call(xmlDoc.getElementsByTagName("item"), function (element) {
-                items.push({
-                    title: element.getElementsByTagName('title')[0].textContent,
-                    link: element.getElementsByTagName('link')[0].textContent,
-                    description: element.getElementsByTagName('description')[0].textContent
-                });
-            });
-
-            for (let item of items) {
-                html += '<li class="list-group-item"><h2><a href="' + item.link + '">' + item.title + '</a></h2><p>' + item.description +'</p></li>';
-            }
-
-            html = '<ul class="list-group">' + html + '</ul>';
-
-            document.getElementById('app').innerHTML = html;
-        } else {
-            console.log('method not supported');
+        for (let item of rss.items) {
+            html += '<li class="list-group-item"><h2><a href="' + item.link + '">' + item.title + '</a></h2><p>' + item.description +'</p></li>';
         }
+
+        html = '<ul class="list-group">' + html + '</ul>';
+
+        document.getElementById('app').innerHTML = html;
     }
 
     var feed = 'https://www.lemonde.fr/rss/une.xml';
-    ajaxRequest(feed);
+    fetchFeed(feed);
 }());
 
